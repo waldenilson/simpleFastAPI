f794ede8167dab23893bbace451244cadf8e0770
Waldenilson Carneiro <waldenilson.carneir@gmail.com>
2025-11-18 11:03:22 -0300

    fist endpoint

--- core_routers.py
+++ core_routers.py
@@ -1,3 +1,7 @@
 from fastapi import APIRouter
 
 core_router = APIRouter(prefix="/core",tags=["Rotas centrais"])
+
+@core_router.get("/")
+async def core():
+    return {"Mensagem":"Acessando Endpoints."}
