d634de799dd26d6a02170e6fc699eaa3ef37e7bd
Waldenilson Carneiro <waldenilson.carneir@gmail.com>
2025-11-18 10:50:00 -0300

    init routers

---
+++ core_routers.py
@@ -0,0 +1,3 @@
+from fastapi import APIRouter
+
+core_router = APIRouter(prefix="/core",tags=["Rotas centrais"])
--- main.py
+++ main.py
@@ -2,3 +2,9 @@
 #github = https://github.com/waldenilson/simpleFastAPI.git
 from fastapi import FastAPI
 app = FastAPI()
+
+from auth_routers import auth_router
+from core_routers import core_router
+
+app.include_router(auth_router)
+app.include_router(core_router)
